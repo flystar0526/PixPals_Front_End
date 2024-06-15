360521aa2be35647a13af06a122c770392458f96
flystar0526 <flowermetoer@gmail.com>
2024-06-15 17:41:48 +0800

    update login, profile_edit ui

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,7 +1,126 @@
-import { Empty } from "antd";
+import { Row, Col, Form, Flex, Input, Button, Typography } from "antd";
+import { LockOutlined, MailOutlined } from "@ant-design/icons";
+
+const { Title } = Typography;
 
 const Login: React.FC = () => {
-  return <Empty description={<span>這是登入頁面</span>} />;
+  return (
+    <Row
+      justify="center"
+      align="middle"
+      style={{
+        width: "100%",
+        height: "100%",
+        backgroundColor: "#f5f5f5",
+      }}
+    >
+      <Col
+        span={12}
+        style={{
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        <Flex
+          justify="flex-end"
+          align="flex-end"
+          style={{
+            width: "100%",
+            height: "100%",
+          }}
+        >
+          <img src="src/assets/phone.png" alt="login.png"></img>
+        </Flex>
+      </Col>
+      <Col
+        span={12}
+        style={{
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        <Flex
+          justify="center"
+          align="flex-start"
+          vertical
+          style={{
+            width: "100%",
+            height: "100%",
+          }}
+        >
+          <Flex
+            vertical
+            justify="center"
+            style={{
+              padding: "64px",
+              width: "30%",
+              backgroundColor: "#ffffff",
+              borderRadius: "16px",
+            }}
+          >
+            <Flex
+              justify="center"
+              align="center"
+              style={{
+                width: "100%",
+              }}
+            >
+              <Title>PixPals</Title>
+            </Flex>
+            <Flex
+              justify="center"
+              align="center"
+              style={{
+                width: "100%",
+              }}
+            >
+              <Form
+                name="login_form"
+                style={{
+                  width: "100%",
+                }}
+              >
+                <Form.Item name="Email">
+                  <Input
+                    size="large"
+                    prefix={<MailOutlined />}
+                    placeholder="電子郵件"
+                  />
+                </Form.Item>
+                <Form.Item name="Password">
+                  <Input
+                    size="large"
+                    prefix={<LockOutlined />}
+                    placeholder="密碼"
+                  />
+                </Form.Item>
+                <Form.Item>
+                  <Button
+                    type="primary"
+                    htmlType="submit"
+                    style={{
+                      width: "100%",
+                    }}
+                  >
+                    登錄
+                  </Button>
+                </Form.Item>
+              </Form>
+            </Flex>
+            <Flex
+              justify="center"
+              align="center"
+              style={{
+                width: "100%",
+              }}
+            >
+              <p>還沒有帳號？點擊註冊</p>
+            </Flex>
+          </Flex>
+        </Flex>
+      </Col>
+    </Row>
+  );
 };
 
 export default Login;
--- src/views/ProfileEdit.tsx
+++ src/views/ProfileEdit.tsx
@@ -1,21 +1,70 @@
-import { Typography, Flex } from "antd";
+import { Flex, Form, Input, Radio, Button, Avatar } from "antd";
+import { UserOutlined } from "@ant-design/icons";
 
-const { Title } = Typography;
+const { TextArea } = Input;
 
 const ProfileEdit: React.FC = () => {
   return (
     <Flex
       justify="flex-start"
       align="flex-start"
-      gap="small"
+      gap="large"
       vertical
       style={{
-        padding: "50px 300px",
+        padding: "50px 500px",
         width: "100%",
         height: "100%",
       }}
     >
-      <Title level={2}>編輯個人資料</Title>
+      <Flex
+        align="center"
+        style={{
+          width: "100%",
+          height: "10%",
+        }}
+      >
+        <h1>編輯個人資料</h1>
+      </Flex>
+      <Avatar size={80} icon={<UserOutlined />} />
+      <Form
+        name="profile_edit"
+        style={{
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        <Form.Item label="名稱" name="username">
+          <Input size="large" />
+        </Form.Item>
+        <Form.Item label="密碼" name="password">
+          <Input size="large" />
+        </Form.Item>
+        <Form.Item label="簡介" name="self_introduction">
+          <TextArea
+            showCount
+            maxLength={300}
+            style={{
+              height: 120,
+              resize: "none",
+            }}
+          />
+        </Form.Item>
+        <Form.Item label="電話" name="phone_number">
+          <Input size="large" />
+        </Form.Item>
+        <Form.Item label="性別" name="gender">
+          <Radio.Group>
+            <Radio value="man"> 男 </Radio>
+            <Radio value="woman"> 女 </Radio>
+          </Radio.Group>
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            儲存
+          </Button>
+          <Button htmlType="submit">取消</Button>
+        </Form.Item>
+      </Form>
     </Flex>
   );
 };
